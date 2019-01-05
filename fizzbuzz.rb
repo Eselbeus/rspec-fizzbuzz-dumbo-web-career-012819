3a28548e2bda0d8a0485203bf30733fc7db3f607
eselbeus <eselbeus@gmail.com>
2019-01-05 21:02:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-fizzbuzz(num)
+def fizzbuzz(num)
   if num % 15
     return "FizzBuzz"
   elsif num % 3
