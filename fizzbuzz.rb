c92444f35bb03a754119f62e0e43be3eebacb9fd
eselbeus <eselbeus@gmail.com>
2019-01-05 21:02:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+fizzbuzz(num)
+  if num % 15
+    return "FizzBuzz"
+  elsif num % 3
+    return "Fizz"
+  elsif num % 5
+    return "Buzz"
+  else
+    return nil
+  end
+end
